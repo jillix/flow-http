cfdd1ed8fcde38f7e6dad22ce836309ef7163094
Adrian Ottiker <adrian@ottiker.com>
2016-07-01 13:53:23 +0000

    remove session related code

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 var http = require('spdy');
 var path = require('path');
 var fs = require('fs');
-var sessions = require('client-sessions');
 var concat = require('concat-stream');
 var cwd = process.cwd();
 var servers = {};
 
-// default session config
-var crypto = require('crypto');
-var defaultSession = {
-    cookieName: 'SES', // cookie name dictates the key name added to the request object
-    requestKey: 'session', // requestKey overrides cookieName for the key name added to the request object
-    secret: crypto.randomBytes(64).toString('hex'), // should be a large unguessable string
-    duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
-    activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-    cookie: {
-        ephemeral: false, // when true, cookie expires when the browser closes
-        httpOnly: true, // when true, cookie is not accessible from javascript
-        secure: true // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process
-    },
-
-    // flow app related configs
+// flow app related configs
+var defaultConfig = {
     wildcard: '*',
     role: 'role',
     user: 'user',
@@ -56,7 +42,7 @@ function getSslInfo (ssl) {
 
 exports.start = function (options, data, next) {
 
-    options._ = Object.assign(defaultSession, options._);
+    options._ = Object.assign(defaultConfig, options._);
     var sslError;
     if ((sslError = getSslInfo(options._.ssl)) instanceof Error) {
         return next(sslError);
@@ -75,24 +61,18 @@ exports.start = function (options, data, next) {
     }
 
     var instance = this;
-    var clientSession = sessions(options._);
 
     servers[port] = http.createServer(options._.ssl, function (req, res) {
-
-        // use encrypted client sessions
-        clientSession(req, res, function () {
-            instance.flow('http_req').write({
-                req: req,
-                res: res,
-                session: req.session
-            });
-        });
+		instance.flow('http_req').write({
+			req: req,
+			res: res,
+		});
     })
 
     // start http server
     servers[port].listen(port, function () {
         console.log('flow-http is listening on port', port);
-        next(null, {server: servers[port], session: clientSession});
+        next(null, {server: servers[port]});
     });
 };
 
