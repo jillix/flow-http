75b326c8abc4fbb273c337b93764f1f4847b6193
Adrian Ottiker <adrian@ottiker.com>
2016-11-17 15:45:55 +0100

    use spdy with ssl

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const servers = {};
 
 exports.listen = (scope, inst, args, data, next) => {
 
-    // TODO get ssl key and cert
     const ssl = SSL.get(scope.env.ssl);
     if (!ssl) {
         return next(new Error('Flow-http.ssl: No ssl info found in environment.'));
     }
 
-    const port = Number.parseInt(scope.env.port || args.port);
+    const port = Number.parseInt(scope.env.port || data.port);
     if (isNaN(port) || port < 1 || port > 65535) {
         return next(new Error('Flow-http: The port option is not a valid port number or out of range.'));
     }
@@ -24,24 +23,14 @@ exports.listen = (scope, inst, args, data, next) => {
         return next(null, data);
     }
 
-    servers[port] = http.createServer((req, res) => Request(scope, inst, args, req, res));
+    servers[port] = http.createServer(ssl, (req, res) => Request(scope, inst, args, req, res));
     servers[port].listen(port, () => next(null, 'Flow-http is listening on port:' + port + '\n'));
     servers[port].on('close', () => servers[port] = null);
 };
 
-// pipe stream to response
-exports.pipe = (scope, inst, args, data, next) => {
-    // TODO check data properties
-    Response.pipe(data.stream, data.res);
-    next(null, data);
-};
-
-// send data to response
-exports.send = (scope, inst, args, data, next) => {
-    // TODO check data properties
-    Response.send(data.headers, data.body, data.res, data.close);
-    next(null, data);
-};
-
-exports.headers = (scope, inst, args, data, next) => {}
-exports.body = (scope, inst, args, data, next) => {}
+exports.status = Response.status;
+exports.headers = Response.headers;
+exports.trailers = Response.trailers;
+exports.pipe = Response.pipe;
+exports.send = Response.send;
+exports.end = Response.end;
